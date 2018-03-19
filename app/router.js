72f5ed45e88d8f21b4c0bb436dc80eff1a4426c9
Andrew Kirwin <andrew.kirwin@zestia.com>
2018-03-19 11:14:11 +0000

    initial example

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('foo', { path: '/foo/:id' });
 });
 
 export default Router;
---
+++ app/routes/foo.js
@@ -0,0 +1,61 @@
+import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
+import jQuery from 'jquery';
+import { later } from '@ember/runloop';
+const { Promise, hash } = RSVP;
+
+export default Route.extend({
+  model() {
+    return hash({
+      foo: this._loadFoo(),
+      bar: this._loadBar()
+    });
+  },
+
+  setupController(controller, model) {
+    controller.setProperties(model);
+  },
+
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      console.log('unloading foo');
+      controller.get('foo').unloadRecord();
+    }
+  },
+
+  _loadFoo() {
+    const foo = this.get('store').peekRecord('foo', 1);
+    if (foo) {
+      console.log('peeking foo');
+      return foo;
+    } else {
+      console.log('finding foo');
+      return this.get('store').findRecord('foo', 1);
+    }
+  },
+
+  _loadBar() {
+    return new Promise(resolve => {
+      later(resolve, 250);
+    });
+  },
+
+  actions: {
+    refresh() {
+      this.refresh();
+    }
+  }
+});
+
+jQuery.mockjax({
+  url: '/foos/1',
+  responseText: {
+    data: {
+      id: 1,
+      type: 'foo',
+      attributes: {
+        name: 'Foo 1'
+      }
+    }
+  }
+});
